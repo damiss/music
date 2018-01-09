fc80ac2c0da97ab229b4c41fa79420149b3beb5b
damiss <296188842@qq.com>
2018-01-09 21:17:59 +0800

    layer

---
+++ src/components/layer/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Layer extends Component {
+    constructor(props) {
+        super(props);
+
+        
+    }
+    render() {
+        return (
+            <div>
+                <div>
+                    layer
+                </div>
+            </div>
+        )
+    }
+}
